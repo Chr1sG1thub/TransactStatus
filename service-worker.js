3c22a38de309a494a47028dd1f81c2581a771f6e
Chr1sG1thub <140241398+Chr1sG1thub@users.noreply.github.com>
2025-12-05 10:26:01 +0100

    x

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,7 @@
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('v1').then(cache =>
-      cache.addAll([
-        '/TransactStatus/index.html',
-        '/TransactStatus/Transact.png'
-      ])
-    )
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
 });
