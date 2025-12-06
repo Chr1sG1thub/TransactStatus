5ceaaab92f6afd8ba15b1f9a89907d5da46c1581
Chr1sG1thub <140241398+Chr1sG1thub@users.noreply.github.com>
2025-12-06 09:13:06 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,13 @@
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  // No files to cache
+  self.skipWaiting(); // Optional: activate immediately
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  self.clients.claim(); // Take control of pages immediately
+});
+
+self.addEventListener('fetch', event => {
+  // Always fetch from network, no caching
+  event.respondWith(fetch(event.request));
 });
